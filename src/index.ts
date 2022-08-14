cd0eecf8c53c48b227bafedd341c3d9b829ff1b7
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-08-13 23:57:23 -0300

    Creating endpoints and prisma client

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import express, {Request, Response} from 'express'
+import {prisma} from './prisma'
+
+const app = express()
+app.use(express.json())
+
+app.post("/", async(req: Request, res: Response)=> {
+    const {name, password} = req.body
+
+    const user = await prisma.user.create({
+        data: {
+            name,
+            password
+        }
+    })
+
+    res.json(user)
+})
+
+app.get("/", async(req: Request, res: Response)=> {
+    const users = await prisma.user.findMany()
+
+    res.json(users)
+})
+
+app.get("/:id", async (req: Request, res: Response) => {
+
+})
+
+app.patch("/", async(req: Request, res: Response)=> {
+    const {id, name} = req.body
+    const updatedUser = await prisma.user.update({
+        where: {
+            id
+        },
+        data: {
+            name
+        }
+    })
+
+    res.json(updatedUser)
+})
+
+app.delete("/:id", async(req: Request, res: Response)=> {
+    const {id} = req.params
+
+    const deletedUser = await prisma.user.delete({
+        where: {
+            id: Number(id)
+        }
+    })
+
+    res.json(deletedUser)
+})
+
+
+app.listen(3001, () => {
+    console.log("Server running")
+})
